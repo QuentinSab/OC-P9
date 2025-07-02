e09aabdcac7d36e3c20366c17b37d635abda190c
QuentinSab <quentinsab@hotmail.fr>
2025-07-02 09:51:48 +0200

    add Ticket, Review and UserFollows models and add Pillow

--- litrevu/webapp/reviews/admin.py
+++ litrevu/webapp/reviews/admin.py
@@ -1,10 +1,26 @@
 from django.contrib import admin
 
 from authentication.models import User
+from reviews.models import Ticket, Review, UserFollows
 
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ("username", "first_name", "last_name", "email")
 
 
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ("title", "user", "time_created")
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("ticket", "headline", "user", "time_created")
+
+
+class UserFollowsAdmin(admin.ModelAdmin):
+    list_display = ("user", "followed_user")
+
+
 admin.site.register(User, UserAdmin)
+admin.site.register(Ticket, TicketAdmin)
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(UserFollows, UserFollowsAdmin)
